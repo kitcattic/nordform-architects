4524479b9fe864ff904dd6f323d91711a954ce77
Kitcat <andreykat0818mix@gmail.com>
2026-08-17 22:47:20 +0400

    Add togge menu and page animathion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,67 @@
-/* Hero Slider */
+/* Togge Menu */
+function initMenu() {
+    const button = document.querySelector(".menu-toggle");
+    const nav = document.querySelector(".nav");
+    const links = nav.querySelectorAll("a");
 
+    if (!button || !nav) return;
+
+    function closeMenu() {
+        button.classList.remove("active");
+        nav.classList.remove("active");
+        button.setAttribute("aria-expanded", "false");
+        document.body.style.overflow = "";
+    }
+
+    button.addEventListener("click", () => {
+        const isOpen = nav.classList.toggle("active");
+
+        button.classList.toggle("active");
+
+        button.setAttribute("aria-expanded", isOpen);
+
+        document.body.style.overflow = isOpen ? "hidden" : "";
+    });
+
+    links.forEach((link) => {
+        link.addEventListener("click", closeMenu);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    initHeroSlider();
+    initScrollAnimation();
+    initMenu();
+});
+
+/* Scroll Animathion */
+function initScrollAnimation() {
+    const elements = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("active");
+                }
+            });
+        },
+        {
+            threshold: 0.15,
+        },
+    );
+
+    elements.forEach((element) => {
+        observer.observe(element);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    initHeroSlider();
+    initScrollAnimation();
+});
+
+/* Hero Slider */
 function initHeroSlider() {
     const slider = document.querySelector(".hero-slider");
 
@@ -12,7 +74,7 @@ function initHeroSlider() {
     let currentSlide = 0;
 
     function showSlide(index) {
-        slides.forEach(slide => {
+        slides.forEach((slide) => {
             slide.classList.remove("active");
         });
 
@@ -48,11 +110,11 @@ function initHeroSlider() {
 
     let startX = 0;
 
-    slider.addEventListener("touchstart", event => {
+    slider.addEventListener("touchstart", (event) => {
         startX = event.touches[0].clientX;
     });
 
-    slider.addEventListener("touchend", event => {
+    slider.addEventListener("touchend", (event) => {
         const endX = event.changedTouches[0].clientX;
         const distance = startX - endX;
 
@@ -70,13 +132,13 @@ document.addEventListener("DOMContentLoaded", () => {
     initHeroSlider();
 });
 
-
+/* Feature Image */
 const featureImageElement = document.querySelector(".feature-image__img");
 
 const featureImages = [
     "assets/images/feature-pool.webp",
     "assets/images/feature-model.webp",
-    "assets/images/feature-exterior.webp"
+    "assets/images/feature-exterior.webp",
 ];
 
 let currentImage = 0;
